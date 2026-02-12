3b4f389691e05ce644593fcafcd1a7d0e996f14e
AhmdHosni <ahmdhosny@gmail.com>
2026-02-12 08:08:17 +0200

    updated exports.zsh

---
+++ scripts/configs/zsh/zshExtras/exports/exports.zsh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# adding android studio exports for zsh to Storage driver instead of the default home driver
+# this needs to be sourced to .zshrc for both home and root.
+
+
+
+#########################################
+# adding path to android main directories
+#########################################
+
+# adding android SDK directory
+export  ANDROID_STUDIO_DIR="/media/ahmdhosni/Storage/Apps/Android"
+
+export ANDROID_HOME="$ANDROID_STUDIO_DIR/Sdk"       # Sets the path to the SDK installation directory
+export ANDROID_USER_HOME=$ANDROID_HOME/.android     # Sets the path to the user preferences directory for tools that are part of the Android SDK. Defaults to $HOME/.android/. 
+export GRADLE_USER_HOME=$ANDROID_HOME/gradle/.gradle    
+#export STUDIO_PROPERTIES=$ANDROID_HOME/custom_options/idea.properties
+#export ANDROID_PREFS_ROOT=$ANDROID_HOME/prefs_root
+#export ANDROID_AVD_HOME=$ANDROID_EMULATOR_HOME/avd
+
+PLATFORM_TOOLS_PATH="$ANDROID_HOME/platform-tools"
+TOOLS_PATH="$ANDROID_HOME/tools"
+FLUTTER_PATH="$ANDROID_HOME/flutter/bin"
+
+
+
+# adding to $PATH
+export PATH=$PATH:$ANDROID_HOME:$ANDROID_USER_HOME:$GRADLE_USER_HOME:$PLATFORM_TOOLS_PATH:$TOOLS_PATH:$FLUTTER_PATH
+
+# adding flutter to PATH 
+#export PATH=$ANDROID_HOME/flutter/bin:$PATH
+# adding tools and platform-tools to PATH
+#export PATH=$ANDROID_HOME/tools:$PATH
+#export PATH=$ANDROID_HOME/platform-tools:$PATH
