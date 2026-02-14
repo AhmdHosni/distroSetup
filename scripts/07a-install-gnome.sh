4563624199d5b20b8590d78c84ae5028f26a0ead
AhmdHosni <ahmdhosny@gmail.com>
2026-02-14 12:03:13 +0200

    Cleaning debian system after update

--- scripts/07a-install-gnome.sh
+++ scripts/07a-install-gnome.sh
@@ -41,6 +41,14 @@ echo -e "${CYAN}${BOLD}Total packages to process: ${TOTAL_PACKAGES}${NC}"
 show_title "INSTALLING GNOME CORE PACKAGES ON $DISTRO"
 
 #########################
+# Clean the debian system:
+#########################
+if command -v apt-get &>/dev/null; then
+    # clean the system
+    sudo apt-get autoremove -y
+fi
+
+#########################
 # INSTALL GNOME PACKAGES:
 #########################
 
@@ -159,6 +167,28 @@ if command -v apt-get &>/dev/null; then
         echo -e "${RED}✗ attempt for the installaion of latest version of Firefox failed${NC}"
         echo -e "${YELLOW}  You can install it manually later${NC}"
     fi
+
+    #!/bin/bash
+    # Script to add Terminal Mode to Debian GRUB
+
+    cat <<'EOF' | sudo tee -a /etc/grub.d/40_custom
+
+menuentry 'Debian GNU/Linux (Terminal Mode)' --class debian --class gnu-linux {
+    load_video
+    insmod gzio
+    insmod part_gpt
+    insmod btrfs
+    search --no-floppy --fs-uuid --set=root 440a4762-4a3d-4d57-b2c8-94cdeb5f6081
+    echo    'Loading Linux 6.12.69+deb13-amd64 (CLI)...'
+    linux   /@/boot/vmlinuz-6.12.69+deb13-amd64 root=UUID=440a4762-4a3d-4d57-b2c8-94cdeb5f6081 ro rootflags=subvol=@ quiet systemd.unit=multi-user.target
+    echo    'Loading initial ramdisk ...'
+    initrd  /@/boot/initrd.img-6.12.69+deb13-amd64
+}
+EOF
+
+    # Update GRUB configuration
+    sudo update-grub
+
 elif command -v pacman &>/dev/null; then
     # Arch spicific package names
     install_package "gdm" "Display Manager: Handles the login screen and user session switching"
@@ -169,7 +199,30 @@ elif command -v pacman &>/dev/null; then
     # for system monitor next extension
     install_package "libgtop" "GObject introspection data for libgtop"
     # install_package "libarchive" "LibArchive: gnome archiving tool,(badunzip, bsdtar ...etc). A utility for compressing and decompressing"
-#install_package "clutter" "GObject introspection data for Clutter"
+    #install_package "clutter" "GObject introspection data for Clutter"
+
+    #!/bin/bash
+    # Script to add Terminal Mode to Arch Linux GRUB
+
+    cat <<'EOF' | sudo tee -a /etc/grub.d/40_custom
+
+menuentry 'Arch Linux (Terminal Mode)' --class arch --class gnu-linux {
+    load_video
+    set gfxpayload=keep
+    insmod gzio
+    insmod part_gpt
+    insmod fat
+    search --no-floppy --fs-uuid --set=root E802-AD2C
+    echo    'Loading Linux linux-lts (CLI)...'
+    linux   /vmlinuz-linux-lts root=UUID=b71b6889-5942-46de-ab4b-1cf6185506eb rw rootflags=subvol=@ zswap.enabled=0 rootfstype=btrfs loglevel=3 quiet systemd.unit=multi-user.target
+    echo    'Loading initial ramdisk ...'
+    initrd  /intel-ucode.img /initramfs-linux-lts.img
+}
+EOF
+
+    # Update GRUB configuration
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 fi
 
 show_title "Installation Complete!" "GNOME 48/49 is ready for use."
