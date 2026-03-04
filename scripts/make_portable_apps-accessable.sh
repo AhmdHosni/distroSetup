5afb94ed01a19b20c2f3f11d0f47e3c5bbffa461
AhmdHosni <ahmdhosny@gmail.com>
2026-03-04 19:58:40 +0200

    making portable apps accessable

---
+++ scripts/make_portable_apps-accessable.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#--------------------------------------------------------------------------------
+# File:          make-it-accessable.sh
+# Created:       Thursday, 19 February 2026 - 01:40 AM
+# Author:        AhmdHosni (ahmdhosny@gmail.com)
+# Link:
+# Description:   A script to make gimp accessable from commandline
+#--------------------------------------------------------------------------------
+
+GIMP_DIR="/media/ahmdhosni/Storage/Apps/gimp"
+VSCODE_DIR="/media/ahmdhosni/Storage/Apps/vsCode/VSCode"
+GLOBAL_BIN_FOLDER="/usr/local/bin/"
+
+sudo ln -s $GIMP_DIR/GIMP.AppImage $GLOBAL_BIN_FOLDER/gimp
+sudo ln -s $VSCODE_DIR/cdoe $GLOBAL_BIN_FOLDER/code
+
+exit 0
